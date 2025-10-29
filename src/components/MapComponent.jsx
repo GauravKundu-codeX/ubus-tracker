d2fb2a40e4b0051238e9bd8f3868189c4200cee2
Gaurav Kundu <gaurav0283.becse24@chitkara.edu.in>
2025-10-29 23:09:20 +0530

    changed bus icon

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -4,13 +4,13 @@ import L from 'leaflet';
 
 // This is a custom bus icon made with SVG
 const busIcon = L.divIcon({
-  html: `<svg viewBox="0 0 24 24" fill="#FFFFFF" stroke="#007bff" stroke-width="1.5" xmlns="http://www.w3.org/2000/svg">
-    <path d="M4 17C4 18.1046 4.89543 19 6 19H7C7.55228 19 8 18.5523 8 18V16C8 15.4477 7.55228 15 7 15H5C4.44772 15 4 15.4477 4 16V17Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M16 17C16 18.1046 16.8954 19 18 19H19C19.5523 19 20 18.5523 20 18V16C20 15.4477 19.5523 15 19 15H17C16.4477 15 16 15.4477 16 16V17Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M3 16V10C3 8.34315 4.34315 7 6 7H18C19.6569 7 21 8.34315 21 10V16" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M3 11H21" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M7 7V5C7 4.44772 7.44772 4 8 4H16C16.5523 4 17 4.44772 17 5V7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-  </svg>`,
+  html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" stroke="#007bff" stroke-width="1.5">
+  <path d="M9 22V12h6V22" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M3 12V6C3 4.89543 3.89543 4 5 4H19C20.1046 4 21 4.89543 21 6V12" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M3 12H21" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M19 16H17.5C16.9477 16 16.5 16.4477 16.5 17V17C16.5 17.5523 16.9477 18 17.5 18H19V16Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M5 16H6.5C7.05228 16 7.5 16.4477 7.5 17V17C7.5 17.5523 7.05228 18 6.5 18H5V16Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
   className: 'bus-icon',
   iconSize: [36, 36],
   iconAnchor: [18, 18], // Center of the icon
